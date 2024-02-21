45a162f588e4d419f18a8b64e2b2931ccccdf79f
Jose Quijada <joquijada2010@gmail.com>
2024-02-21 09:40:17 -0500

    feat(spring): initial commit, thing builds

---
+++ app/src/test/java/com/exsoinn/spring/practice/SpringPracticeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.exsoinn.spring.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringPracticeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
