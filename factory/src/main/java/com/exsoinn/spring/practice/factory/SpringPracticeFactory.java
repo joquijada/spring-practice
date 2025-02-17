569883cdbedc43a6c4b241309553d50750d486eb
Jose Quijada <joquijada2010@gmail.com>
2025-02-17 16:39:26 -0500

    chore(factory): rename to keep naming convention consistent

--- factory/src/main/java/com/exsoinn/spring/practice/factory/SpringPracticeFactory.java
+++ factory/src/main/java/com/exsoinn/spring/practice/factory/SpringPracticeFactory.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPracticeApplication {
+public class SpringPracticeFactory {
     public static void main(String[] args) {
         SpringApplication.run(SpringPracticeApplication.class, args);
     }
